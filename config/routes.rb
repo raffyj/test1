321f958c993a34beadbac54eae62a23d05423e11
Raffy <raffyjara@gmail.com>
2012-01-25 16:03:42 +0800

    layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,19 @@
 Test1::Application.routes.draw do
-  get "pages/home"
-  get "pages/contact"
-  get "pages/about"
+  get "users/new"
+
+  match '/contact', :to => 'pages#contact'
+  match '/about', :to => 'pages#about'
+  match '/help', :to => 'pages#help'
+  match '/signup', :to => 'users#new'  
+  
+  # get "pages/home"
+  # get "pages/contact"
+  # get "pages/about"
+  # get "pages/help"
+  
+  # You can have the root of your site routed with "root"
+  # just remember to delete public/index.html.
+  root :to => 'pages#home'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
---
+++ spec/requests/layout_links_spec.rb
@@ -0,0 +1,46 @@
+﻿require 'spec_helper'
+
+describe "LayoutLinks" do
+
+  it "should have a Home page at '/'" do
+    get '/'
+	response.should have_selector('title', :content => "HOME")
+  end
+  
+  it "should have a Contact page at '/contact'" do
+    get '/contact'
+	response.should have_selector('title', :content => "CONTACT")
+  end
+  
+  it "should have an About page at '/about'" do
+    get '/about'
+	response.should have_selector('title', :content => "ABOUT")
+  end
+  
+  it "should have a Help page at '/help'" do
+    get '/help'
+	response.should have_selector('title', :content => "HELP")
+  end
+
+  it "should have a Signup page at '/signup'" do
+    get '/signup'
+	response.should have_selector('title', :content => "SIGN UP")
+  end
+
+  it "should have the right links on  the layout" do
+    visit root_path
+	response.should have_selector('title', :content => "HOME")
+	click_link "About"
+	response.should have_selector('title', :content => "ABOUT")
+	click_link "Contact"
+	response.should have_selector('title', :content => "CONTACT")	
+	click_link "Home"
+	response.should have_selector('title', :content => "ABOUT")
+	click_link "Sign up now!"
+	response.should have_selector('title', :content => "SIGN UP")
+	
+	response.should have_selector('a[href="/"]>img')
+  end
+  
+end
+
