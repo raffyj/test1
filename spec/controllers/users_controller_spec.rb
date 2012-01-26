6e21e13470ddefe085b7a0504efa24c384c8ca98
Raffy <raffyjara@gmail.com>
2012-01-26 21:21:19 +0800

    user page

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -18,17 +18,35 @@ describe UsersController do
 	  get :show, id => @user
 	  assigns(:user).should == @user
 	end
-
-	describe "GET 'new'" do
-    it "returns http success" do
-      get :new
-      response.should be_success
-    end
 	
-	it "should have the right title" do
-	  get :new
-	  response.should have_selector('title', :content => "SIGN UP")
+	it "should have the right titile" do
+	  get :show, id => @user
+	  response.should have_selector('title', :content => @user.name)
 	end
+	  
+	it "should have the user's name" do
+	  get :show, :id => @user
+	  response.should have_selector('h1', :content => @user.name)
+	end
+	
+	it "should have a profile image" do
+	  get :show, :id => @user
+	  response.should have_selector('h1>img', :class => @gravatar)
+	end
+	
+  end
+	
+
+  describe "GET 'new'" do
+      it "returns http success" do
+        get :new
+        response.should be_success
+      end
+	
+	  it "should have the right title" do
+	    get :new
+        response.should have_selector('title', :content => "SIGN UP")
+	  end
   end
 
 end
